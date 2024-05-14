8242d5ac663b940fd2abf2d41f65b08a5634dd41
berreys <saberrey@gmail.com>
2024-05-13 23:16:34 -0600

    real names added to dropdown

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function submitForm() {
     var selectedName = document.getElementById("selectName").value;
+    if(selectedName.length === 0){
+        return;
+    }
     window.location.href = "person.html?name=" + encodeURIComponent(selectedName);
 }
